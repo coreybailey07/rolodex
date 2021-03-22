d9ff1aea8bd8d41bc1dfb6a10fc910aa0c0f3af0
Corey Bailey <public@coreybailey.net>
2021-03-22 13:48:37 -0400

    Remove parenthesis

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './card.styles.css';
 
-export const Card = (props) => (
+export const Card = props => (
   <div className='card-container'>
     <img
       alt='monster'
